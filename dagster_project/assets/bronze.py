cfa0e74137832fcaefb682ae69a6a4e868d4522f
ReMeiradL <renatameiradl@hotmail.com>
2026-08-25 15:28:39 -0300

    Update bronze.py

--- dagster_project/assets/bronze.py
+++ dagster_project/assets/bronze.py
@@ -6,6 +6,6 @@ NOME_ARQUIVO = "preco_primeiro_semestre_2026.csv"
 
 
 @asset(description="Dados tecnicos em Parquet: colunas renomeadas, ainda sem limpeza de valores.")
-def bronze_combustiveis(raw_combustiveis: str) -> str:
+def bronze_combustiveis() -> str:
     caminho = gerar_bronze(NOME_ARQUIVO)
     return str(caminho)
